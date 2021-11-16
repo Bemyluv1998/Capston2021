2f43d17c092052379184cbeed2b0001999631692
Bemyluv1998 <kim2563159@gmail.com>
2021-11-17 01:43:55 +0900

    js navbar menu
    js

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 'use strict';
 
+//make navbar transparent when it is on the top
 const navbar =document.querySelector('#navbar');
 const navbarHeight =navbar.getBoundingClientRect().height;
 document.addEventListener('scroll',() =>{
@@ -8,4 +9,20 @@ document.addEventListener('scroll',() =>{
     }else{
         navbar.classList.remove('navbar--dark');
     }
+});
+
+// handle scrolling when tapping on the navbar menu
+
+const navbarMenu= document.querySelector('.navbar__menu');
+navbarMenu.addEventListener('click',(event)=> {
+    console.log(event.target.dataset.link);
+    const target= event.target;
+    const link =target.dataset.link;
+    if(link ==null) {
+        return;
+    }
+
+    console.log(event.target.dataset.link);
+    const scrollTo =document.querySelector(link);
+    scrollTo.scrollIntoView({behavior: 'smooth'});
 });
