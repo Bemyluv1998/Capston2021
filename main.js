0b5d115e7e73f5294c244c8aaa8ee9bce9f174f6
Bemyluv1998 <kim2563159@gmail.com>
2021-11-18 21:52:00 +0900

    how to use js
    how to js 추가

--- main.js
+++ main.js
@@ -26,3 +26,39 @@ navbarMenu.addEventListener('click',(event)=> {
     const scrollTo =document.querySelector(link);
     scrollTo.scrollIntoView({behavior: 'smooth'});
 });
+
+// home transparent as the window scrolls down
+const home =document.querySelector('.home__container');
+const homeHeight = home.getBoundingClientRect().height;
+document.addEventListener('scroll',()=>{
+    home.style.opacity = 1 - window.scrollY /homeHeight;
+});
+
+//party use 
+const workBtnContainer = document.querySelector('.howtouse__categories');
+const projectContainer = document.querySelector('.work__projects');
+const projects =document.querySelectorAll('.project');
+workBtnContainer.addEventListener('click',(e)=>{
+    const filter =e.target.dataset.filter || e.target.parenNode.dataset.filter;
+    if(filter == null){
+        return;
+    }
+    console.log(filter);
+    projects.forEach((project) => {
+        console.log(project.dataset.type);
+        if(filter ==='*' || filter === project.dataset.type){
+            project.classList.remove('invisible');
+        } else{
+            project.classList.add('invisible');
+        }
+        
+    });
+
+});
+
+
+
+function scrollIntoView(selector) {
+        const scrollTo =document.querySelector(selector);
+        scrollTo.scrollIntoView({ behavior: 'smooth'});
+    }
